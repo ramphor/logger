8e1a22001430c706b33a01d3b877b470bccecc8c
Puleeno Nguyen <puleeno@gmail.com>
2022-05-22 17:33:15 +0700

    Do not run in WP CLI mode

--- ramphor-logger.php
+++ ramphor-logger.php
@@ -12,115 +12,117 @@
 use Ramphor\Logger\Logger;
 use Monolog\Handler\SlackHandler;
 
-$composer_file = sprintf( '%s/vendor/autoload.php', dirname( __FILE__ ) );
-if ( file_exists( $composer_file ) ) {
-	require_once $composer_file;
-}
-
-if ( ! class_exists( Logger::class ) ) {
-	return error_log( sprintf( 'The class %s is not found to register custom logger', Logger::class ) );
-}
-
-function load_ramphor_logger_firstly() {
-	$path = substr(
-		__FILE__,
-		strlen( __FILE__ ) - 33
-	);
-
-	// Set me at first index
-	if ( $plugins = get_option( 'active_plugins' ) ) {
-		if ( $key = array_search( $path, $plugins ) ) {
-			array_splice( $plugins, $key, 1 );
-			array_unshift( $plugins, $path );
-			update_option( 'active_plugins', $plugins );
-		}
-	}
-}
-add_action( 'activated_plugin', 'load_ramphor_logger_firstly', 100 );
-
-function ramphor_logger_exception_trigger( $e ) {
-	$message = sprintf(
-		"%s\n%s",
-		$e->getMessage(),
-		$e->getTraceAsString()
-	);
-	$logger  = Logger::instance();
-	$logger->get()->error( $message );
-
-	_e( 'Your website has errors. Please contact to webadmin or your developer to get more informations', 'ramphor_logger' );
-}
-set_exception_handler( 'ramphor_logger_exception_trigger' );
-
-function ramphor_logger_error_trigger( $errno, $errstr, $errfile, $errline, $errcontext = array() ) {
-	$ignore_wp_core = apply_filters(
-		'ramphor_logger_ignore_wordpress_core_warning',
-		true
-	);
-	$errdir         = preg_replace(
-		'/([^\/]+)(.+)?/',
-		'$1',
-		str_replace( ABSPATH, '', $errfile )
-	);
-	if ( $ignore_wp_core && in_array( $errdir, array( 'wp-includes', 'wp-admin' ) ) ) {
-		return;
+if (!class_exists(\WP_CLI::class)) {
+	$composer_file = sprintf( '%s/vendor/autoload.php', dirname( __FILE__ ) );
+	if ( file_exists( $composer_file ) ) {
+		require_once $composer_file;
 	}
 
-	ob_start();
-	debug_print_backtrace( DEBUG_BACKTRACE_IGNORE_ARGS );
-	$backtrace = ob_get_clean();
-	$message   = sprintf( "%s in %s line %s\n%s", $errstr, $errfile, $errline, $backtrace );
-	$logger    = Logger::instance();
-
-	$user_error_log_level = get_option( 'user_error_log_level', 'warning' );
-	if ( is_callable( array( $logger->getLogger(), $user_error_log_level ) ) ) {
-		$logger->$user_error_log_level( $message );
-	} else {
-		$logger->warning( $message );
+	if ( ! class_exists( Logger::class ) ) {
+		return error_log( sprintf( 'The class %s is not found to register custom logger', Logger::class ) );
 	}
-}
-set_error_handler( 'ramphor_logger_error_trigger' );
 
-function ramphor_logger_register_logger( $logger, $id ) {
-	if ( ! defined( 'RAMPHOR_LOGGER_SLACK_HANDLER_ENABLE' ) ) {
-		return;
-	}
-	$token   = defined( 'RAMPHOR_LOGGER_SLACK_TOKEN' ) ? constant( 'RAMPHOR_LOGGER_SLACK_TOKEN' ) : '';
-	$channel = defined( 'RAMPHOR_LOGGER_SLACK_CHANNEL' ) ? constant( 'RAMPHOR_LOGGER_SLACK_CHANNEL' ) : '';
-	$botname = defined( 'RAMPHOR_LOGGER_SLACK_BOT_NAME' ) ? constant( 'RAMPHOR_LOGGER_SLACK_BOT_NAME' ) : '';
-
-	if ( isset( $token, $channel ) ) {
-		$slackHandler  = new SlackHandler( $token, $channel, $botname );
-		$monitor_level = defined('RAMPHOR_LOGGER_SLACK_LOG_LEVEL')
-			? RAMPHOR_LOGGER_SLACK_LOG_LEVEL
-			: get_option( 'ramphor_logger_slack_at_level', $logger::NOTICE );
-
-		$log_level     = $logger::NOTICE;
-		if ( in_array(
-			$monitor_level,
-			array(
-				$logger::DEBUG,
-				$logger::INFO,
-				$logger::NOTICE,
-				$logger::WARNING,
-				$logger::ERROR,
-				$logger::CRITICAL,
-				$logger::ALERT,
-				$logger::EMERGENCY,
-			)
-		) ) {
-			$log_level = $monitor_level;
-		}
-
-		$slackHandler->setLevel(
-			apply_filters(
-				'ramphor_logger_slack_handler_log_level',
-				$log_level
-			)
+	function load_ramphor_logger_firstly() {
+		$path = substr(
+			__FILE__,
+			strlen( __FILE__ ) - 33
 		);
-		$logger->pushHandler( $slackHandler );
-	}
-}
-add_action( 'ramphor_logger_register_logger', 'ramphor_logger_register_logger', 10, 2 );
 
-// Init the Ramphor Logger instance
-add_action( 'loaded_plugins', array( Logger::class, 'instance' ) );
+		// Set me at first index
+		if ( $plugins = get_option( 'active_plugins' ) ) {
+			if ( $key = array_search( $path, $plugins ) ) {
+				array_splice( $plugins, $key, 1 );
+				array_unshift( $plugins, $path );
+				update_option( 'active_plugins', $plugins );
+			}
+		}
+	}
+	add_action( 'activated_plugin', 'load_ramphor_logger_firstly', 100 );
+
+	function ramphor_logger_exception_trigger( $e ) {
+		$message = sprintf(
+			"%s\n%s",
+			$e->getMessage(),
+			$e->getTraceAsString()
+		);
+		$logger  = Logger::instance();
+		$logger->get()->error( $message );
+
+		_e( 'Your website has errors. Please contact to webadmin or your developer to get more informations', 'ramphor_logger' );
+	}
+	set_exception_handler( 'ramphor_logger_exception_trigger' );
+
+	function ramphor_logger_error_trigger( $errno, $errstr, $errfile, $errline, $errcontext = array() ) {
+		$ignore_wp_core = apply_filters(
+			'ramphor_logger_ignore_wordpress_core_warning',
+			true
+		);
+		$errdir         = preg_replace(
+			'/([^\/]+)(.+)?/',
+			'$1',
+			str_replace( ABSPATH, '', $errfile )
+		);
+		if ( $ignore_wp_core && in_array( $errdir, array( 'wp-includes', 'wp-admin' ) ) ) {
+			return;
+		}
+
+		ob_start();
+		debug_print_backtrace( DEBUG_BACKTRACE_IGNORE_ARGS );
+		$backtrace = ob_get_clean();
+		$message   = sprintf( "%s in %s line %s\n%s", $errstr, $errfile, $errline, $backtrace );
+		$logger    = Logger::instance();
+
+		$user_error_log_level = get_option( 'user_error_log_level', 'warning' );
+		if ( is_callable( array( $logger->getLogger(), $user_error_log_level ) ) ) {
+			$logger->$user_error_log_level( $message );
+		} else {
+			$logger->warning( $message );
+		}
+	}
+	set_error_handler( 'ramphor_logger_error_trigger' );
+
+	function ramphor_logger_register_logger( $logger, $id ) {
+		if ( ! defined( 'RAMPHOR_LOGGER_SLACK_HANDLER_ENABLE' ) ) {
+			return;
+		}
+		$token   = defined( 'RAMPHOR_LOGGER_SLACK_TOKEN' ) ? constant( 'RAMPHOR_LOGGER_SLACK_TOKEN' ) : '';
+		$channel = defined( 'RAMPHOR_LOGGER_SLACK_CHANNEL' ) ? constant( 'RAMPHOR_LOGGER_SLACK_CHANNEL' ) : '';
+		$botname = defined( 'RAMPHOR_LOGGER_SLACK_BOT_NAME' ) ? constant( 'RAMPHOR_LOGGER_SLACK_BOT_NAME' ) : '';
+
+		if ( isset( $token, $channel ) ) {
+			$slackHandler  = new SlackHandler( $token, $channel, $botname );
+			$monitor_level = defined('RAMPHOR_LOGGER_SLACK_LOG_LEVEL')
+				? RAMPHOR_LOGGER_SLACK_LOG_LEVEL
+				: get_option( 'ramphor_logger_slack_at_level', $logger::NOTICE );
+
+			$log_level     = $logger::NOTICE;
+			if ( in_array(
+				$monitor_level,
+				array(
+					$logger::DEBUG,
+					$logger::INFO,
+					$logger::NOTICE,
+					$logger::WARNING,
+					$logger::ERROR,
+					$logger::CRITICAL,
+					$logger::ALERT,
+					$logger::EMERGENCY,
+				)
+			) ) {
+				$log_level = $monitor_level;
+			}
+
+			$slackHandler->setLevel(
+				apply_filters(
+					'ramphor_logger_slack_handler_log_level',
+					$log_level
+				)
+			);
+			$logger->pushHandler( $slackHandler );
+		}
+	}
+	add_action( 'ramphor_logger_register_logger', 'ramphor_logger_register_logger', 10, 2 );
+
+	// Init the Ramphor Logger instance
+	add_action( 'loaded_plugins', array( Logger::class, 'instance' ) );
+}
